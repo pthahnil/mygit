c2a18d84b251f8277376afc801357da3b9c32646
pthahnil <xiaoronglee@hotmail.com>
2016-09-12 22:25:38 +0800

    blur

--- lwjgl2.9/src/postProcessing/PostProcessing.java
+++ lwjgl2.9/src/postProcessing/PostProcessing.java
@@ -4,7 +4,10 @@ import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
 
+import gaussianBlur.HorizontalBlur;
+import gaussianBlur.VerticalBlur;
 import models.RawModel;
+import renderEngine.DisplayManager;
 import renderEngine.Loader;
 
 public class PostProcessing {
@@ -13,20 +16,36 @@ public class PostProcessing {
 	private static RawModel quad;
 	
 	private static ContrastChanger changer;
+	private static HorizontalBlur hBlur;
+	private static VerticalBlur vBlur;
+	private static HorizontalBlur hBlur2;
+	private static VerticalBlur vBlur2;
 	
 	public static void init(Loader loader){
 		quad = loader.loadToVAO(POSITIONS, 2);
 		changer = new ContrastChanger();
+		hBlur = new HorizontalBlur(DisplayManager.getWidth()/8, DisplayManager.getHeight()/8);
+		vBlur = new VerticalBlur(DisplayManager.getWidth()/8, DisplayManager.getHeight()/8);
+		hBlur2 = new HorizontalBlur(DisplayManager.getWidth()/2, DisplayManager.getHeight()/2);
+		vBlur2 = new VerticalBlur(DisplayManager.getWidth()/2, DisplayManager.getHeight()/2);
 	}
 	
 	public static void doPostProcessing(int colourTexture){
 		start();
-		changer.render(colourTexture);
+		hBlur2.render(colourTexture);
+		vBlur2.render(hBlur2.getOutputTexture());
+		hBlur.render(vBlur2.getOutputTexture());
+		vBlur.render(hBlur.getOutputTexture());
+		changer.render(vBlur.getOutputTexture());
 		end();
 	}
 	
 	public static void cleanUp(){
-
+		changer.cleanUp();
+		hBlur.cleanUp();
+		vBlur.cleanUp();
+		hBlur2.cleanUp();
+		vBlur2.cleanUp();
 	}
 	
 	private static void start(){
